c31ae421174812939c93df72c4f4d68f5f76acb5
cfalcant <cfalcant@gmail.com>
2018-02-22 14:33:08 -0700

    easy 2 exercise complete

--- react-tdd-easy-2/src/App.js
+++ react-tdd-easy-2/src/App.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    favFoods: [
+      {
+        id:1,
+        item: 'wings'
+      },
+      {
+        id: 2,
+        item: 'pizza'
+      },
+      {
+        id: 3,
+        item: 'spaghetti'
+      }
+    ]
+  }
   render () {
+    let listOfFoodItems = this.state.favFoods.map((food)=>{
+      return (
+          <li
+            key={food.id}>
+            {food.item}
+          </li>
+      )
+    })
     return (
-      <div>App component</div>
+      <div>{listOfFoodItems}</div>
     )
   }
 }
