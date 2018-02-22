ac4acb074c6640bfb1ab53832ada07a90da43a60
cfalcant <cfalcant@gmail.com>
2018-02-22 14:10:49 -0700

    easy 1 exercise complete

--- react-tdd-easy-1/src/App.js
+++ react-tdd-easy-1/src/App.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        objective: 'Wash dishes'
+      },
+      {
+        id: 2,
+        objective: 'Make dinner'
+      },
+      {
+        id: 3,
+        objective: 'Laundry'
+      }
+    ]
+  }
+
   render () {
+    let listOfTodos=this.state.todos.map((todo)=>{
+        return (
+          <li
+            key={todo.id}>
+            {todo.objective}
+          </li>
+        )
+    })
+
     return (
-      <div>App component</div>
+      <div className="App">
+        {listOfTodos}
+      </div>
     )
   }
 }
